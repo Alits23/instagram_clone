cfc1e71dbd741a449ac709f318d7eb7dd53b1bff
Ali Tashakori <ali.tashakori.23@gmail.com>
2023-07-11 01:37:41 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/pattern1.png'),
+          repeat: ImageRepeat.repeat,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(80.0),
+              child: Center(
+                child: Image(
+                  image: AssetImage('images/logo_splash.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 32.0,
+              child: Column(
+                children: [
+                  Text(
+                    'From',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Text(
+                    'Elit Group',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
