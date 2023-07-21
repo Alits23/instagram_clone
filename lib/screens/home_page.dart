8cdcd6bd3583fc2af5d05eb6a4151e9e49e4032f
Ali Tashakori <ali.tashakori.23@gmail.com>
2023-07-21 21:05:28 +0330

    init+

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/constants/colors/colors.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -28,67 +29,280 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 13, left: 17),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                width: 64,
-                height: 64,
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Image(
-                    image: AssetImage('images/icon_plus.png'),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: 394,
+            height: 440,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Positioned(
+                  top: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset('images/post_cover.png'),
                   ),
                 ),
-                decoration: BoxDecoration(
-                  border: Border.all(color: whiteColor, width: 2),
-                  borderRadius: BorderRadius.circular(17),
+                Positioned(
+                  top: 15,
+                  right: 15,
+                  child: Image.asset('images/icon_video.png'),
                 ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              getContainerStory('item4'),
-              getContainerStory('item4'),
-              getContainerStory('item4'),
-              getContainerStory('item4'),
-            ],
+                Positioned(
+                  bottom: 15,
+                  child: Container(
+                    width: 340,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: pinkColorIcon,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset('images/icon_hart.png'),
+                        Image.asset('images/icon_comments.png'),
+                        Image.asset('images/icon_share.png'),
+                        Image.asset('images/icon_save.png'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget getContainerStory(String imageName) {
-    return Row(
-      children: [
-        Container(
-          width: 64,
-          height: 64,
-          decoration: BoxDecoration(
-            border: Border.all(color: pinkColorIcon, width: 2),
-            borderRadius: BorderRadius.circular(17),
+  Widget _getStoryBox() {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: Radius.circular(17),
+      padding: EdgeInsets.all(4),
+      color: pinkColorIcon,
+      dashPattern: [40, 10],
+      strokeWidth: 2,
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        child: Container(
+          width: 38,
+          height: 38,
+          child: Image.asset('images/profile.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _getHeaderPost() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: Row(
+        children: [
+          _getStoryBox(),
+          Padding(
+            padding: const EdgeInsets.only(left: 12),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Ali Tashakori',
+                  style: TextStyle(
+                    fontFamily: 'GB',
+                    fontSize: 12,
+                    color: whiteColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'برنامه نویس فلاتر',
+                  style: TextStyle(
+                    fontFamily: 'SM',
+                    fontSize: 12,
+                    color: whiteColor,
+                  ),
+                ),
+              ],
+            ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(2),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  image: AssetImage('images/${imageName}.png'),
+          Spacer(),
+          Icon(
+            Icons.more_vert_outlined,
+            color: whiteColor,
+          )
+        ],
+      ),
+    );
+  }
+
+/* For me
+  Widget getlistStory() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 17, top: 13, right: 11),
+      child: Container(
+        height: 90,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 15,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 22),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 64,
+                    height: 64,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: pinkColorIcon, width: 2),
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: backgroundColor1,
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage('images/item4.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'data',
+                    style: TextStyle(
+                      fontFamily: 'GS',
+                      fontSize: 10,
+                      color: whiteColor,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget getPost() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 17, right: 13),
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 44,
+                      height: 44,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: pinkColorIcon, width: 2),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: backgroundColor1, width: 2),
+                        ),
+                        child: Image.asset(
+                          'images/profile.png',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ali Tashakori',
+                          style: TextStyle(
+                            fontFamily: 'GB',
+                            fontSize: 12,
+                            color: whiteColor,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'برنامه نویس فلاتر',
+                          style: TextStyle(
+                            fontFamily: 'SM',
+                            fontSize: 12,
+                            color: whiteColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.more_vert_outlined,
+                ),
+                color: whiteColor,
+              ),
+            ],
+          ),
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                width: 394,
+                height: 394,
+                child: Image.asset(
+                  'images/post_cover.png',
                   fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-      ],
+              Positioned(
+                top: 381,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: whiteColor,
+                  ),
+                  width: 340,
+                  height: 46,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
+  */
 }
