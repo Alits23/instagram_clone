2d96387eed25f9f1a1e2482c7fa138fdbae5c627
Ali Tashakori <ali.tashakori.23@gmail.com>
2023-08-04 12:59:09 +0330

    add eleveted button

--- lib/screens/share_bottomsheet.dart
+++ lib/screens/share_bottomsheet.dart
@@ -20,96 +20,144 @@ class ShareBottomSheet extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 40),
           color: whiteColor.withOpacity(0.09),
           height: 300,
-          child: _getContent(),
+          child: _getContent(context),
         ),
       ),
     );
   }
 
-  Widget _getContent() {
-    return CustomScrollView(
-      controller: controller,
-      slivers: [
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              Container(
-                width: 67,
-                height: 5,
-                margin: EdgeInsets.only(top: 10, bottom: 25),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(100),
-                  ),
-                  color: whiteColor,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget _getContent(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        CustomScrollView(
+          controller: controller,
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
                 children: [
-                  Text(
-                    'Share',
-                    style: TextStyle(
-                      fontFamily: 'GB',
-                      fontSize: 20,
+                  Container(
+                    width: 67,
+                    height: 5,
+                    margin: EdgeInsets.only(top: 10, bottom: 25),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(100),
+                      ),
                       color: whiteColor,
                     ),
                   ),
-                  Image.asset('images/icon_share_bottomsheet.png'),
-                ],
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              Container(
-                height: 46,
-                decoration: BoxDecoration(
-                  color: whiteColor.withOpacity(0.4),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(13),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Image.asset('images/icon_search.png'),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Search User',
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
+                      Text(
+                        'Share',
+                        style: TextStyle(
+                          fontFamily: 'GB',
+                          fontSize: 20,
+                          color: whiteColor,
                         ),
                       ),
+                      Image.asset('images/icon_share_bottomsheet.png'),
                     ],
                   ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Container(
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: whiteColor.withOpacity(0.4),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(13),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: [
+                          Image.asset('images/icon_search.png'),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search User',
+                                enabledBorder: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                ],
+              ),
+            ),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return _getItemGrid();
+                },
+                childCount: 28,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 10,
+                mainAxisExtent: 110,
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.only(top: 90),
+            ),
+          ],
+        ),
+        Positioned(
+          bottom: 47,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 45),
+              child: Text(
+                'Send',
+                style: TextStyle(
+                  fontFamily: 'GB',
+                  fontSize: 16,
                 ),
               ),
-              SizedBox(
-                height: 32,
-              ),
-            ],
+            ),
           ),
         ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              return Container(
-                color: pinkColorIcon,
-              );
-            },
-            childCount: 25,
+      ],
+    );
+  }
+
+  Widget _getItemGrid() {
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
           ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
+          child: Image.asset('images/profile.png'),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '@ alits23',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontFamily: 'GB', fontSize: 12, color: whiteColor),
         ),
       ],
     );
